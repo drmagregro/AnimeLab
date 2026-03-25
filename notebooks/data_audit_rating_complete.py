957c665102e96becdcd6a65798bcdb8cd0ad73da
elbitor0 <123994826+elbitor0@users.noreply.github.com>
2026-03-25 13:54:31 +0100

    feature: johanne notebook

---
+++ notebooks/data_audit_rating_complete.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# Audit
+
+#Columns are: user_id,anime_id,rating
+# user_id: User's ID
+# anime_id: Anime's ID
+# rating: Anime rating given par de user
+
+rating_complete_df = pd.read_csv("data/rating_complete.csv")
+
+print(rating_complete_df[
+    pd.to_numeric(rating_complete_df["user_id"], errors="coerce").isna()
+])
+print(rating_complete_df["user_id"].isna().sum())
+print(rating_complete_df[
+    pd.to_numeric(rating_complete_df["anime_id"], errors="coerce").isna()
+])
+print(rating_complete_df["anime_id"].isna().sum())
+print(rating_complete_df[
+    pd.to_numeric(rating_complete_df["rating"], errors="coerce").isna()
+])
+print(rating_complete_df["rating"].isna().sum())
+# No invalid value
